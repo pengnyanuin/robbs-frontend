c751945604b75c58450aeb69f3e0d4355abd214c
matej.snajdr <matej.snajdr@ubk.cz>
2022-09-29 10:57:38 +0200

    dist subtree

--- assets/404.3658d5ae.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o,d as t}from"./index.5ef06ab2.js";const s={},n=t("h1",null,"404",-1),_=[n];function r(a,d){return c(),o("div",null,_)}const f=e(s,[["render",r]]);export{f as default};
