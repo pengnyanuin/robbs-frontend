d66a5c9890fea9c442423f648cc4d59700889e8b
matej.snajdr <matej.snajdr@ubk.cz>
2022-10-03 16:43:28 +0200

    pls fix 2

---
+++ src/views/ActionBoard.vue
@@ -0,0 +1,31 @@
+<template>
+    <draggable
+        class="dragArea"
+        tag="ul"
+        :list="tasks"
+        :group="{ name: 'g1' }"
+        item-key="name"
+    >
+        <template #item="{ element }">
+            <li>
+                <p>{{ element.name }}</p>
+                <nested-draggable :tasks="element.tasks" />
+            </li>
+        </template>
+    </draggable>
+</template>
+<script>
+import draggable from "@/vuedraggable";
+export default {
+    props: {
+        tasks: {
+            required: true,
+            type: Array
+        }
+    },
+    components: {
+        draggable
+    },
+    name: "nested-draggable"
+};
+</script>
---
+++ src/views/components/Robb.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="robb" :class="['rot-' + position.rot, {'current': currentPlayer}]" :style="{position: 'absolute', top: position.top + 'px', left: position.left + 'px'}" ref="thisRob">{{ currentPlayer ? 'me' : id }}</div>
+</template>
+<script>
+
+export default {
+    name: "robb",
+    props: {
+        id: Number,
+        playerPosition: Array,
+        currentPlayer: Boolean
+    },
+    watch: {
+        playerPosition: {
+            deep: true,
+            handler: function (newPosition) {
+                this.position.left = 5 + (newPosition[1] - 1) * 50;
+                this.position.top = 20 + (newPosition[0] - 1) * 50;
+                this.position.rot = newPosition[2] + 1;
+            }
+        }
+    },
+    data() {
+        return {
+            position: {
+                top: 20,
+                left: 5,
+                rot: 0,
+            }
+        }
+    },
+    methods: {
+        rotationClass() {
+            return 'rot-' + this.position.rot;
+        }
+    },
+    mounted() {
+        this.position.left += (this.playerPosition[1] - 1) * 50;
+        this.position.top += (this.playerPosition[0] - 1) * 50;
+        this.position.rot = this.playerPosition[2] + 1;
+    }
+}
+</script>
