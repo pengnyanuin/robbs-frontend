3f0fa502b9374ad662c552c9e43bdf2e7f1238a7
matej.snajdr <matej.snajdr@ubk.cz>
2022-10-11 11:27:28 +0200

    Ably

--- src/Ably.vue
+++ src/Ably.vue
@@ -3,7 +3,7 @@
         <h1>
             aaaaaaaa
         </h1>
-        <div style="border: 1px solid black; height: 500px; width: 800px;" class="mb-5">
+        <div style="border: 1px solid black; height: 500px; width: 800px; overflow: auto" class="mb-5" ref="chat">
             <p v-for="(message, i) in chat" :key="i">
                 <b>{{ message.name }}: </b>
                 <span>{{ message.content }}</span>
@@ -11,14 +11,16 @@
         </div>
         <div style="width: 500px" class="d-flex flex-column">
             <input type="text" v-model="name" class="mb-2">
-            <textarea rows="4" class="mb-4" v-model="message"></textarea>
+            <textarea rows="4" class="mb-4" v-model="message" ref="textarea"></textarea>
             <a href="#" class="btn" @click.prevent="sendMessage()">Send</a>
         </div>
     </div>
 </template>
 <script>
 
+import { nextTick } from 'vue';
 import {AblyService} from "@/services/ably.service";
+// import * as Ably from 'ably';
 
 export default {
     name: "AblyApp",
@@ -35,11 +37,25 @@ export default {
         async sendMessage() {
             this.AblyClass.sendMessage(this.name, this.message);
             this.message = '';
+        },
+        handleMessageFromAbly(message) {
+            this.chat = [
+                ...this.chat,
+                {
+                    name: message.name,
+                    content: message.data
+                }
+            ];
+
+            this.$refs.textarea.focus();
+            nextTick(() => {
+                this.$refs.chat.scrollTop = this.$refs.chat.scrollHeight;
+            });
         }
     },
     mounted() {
-        this.AblyClass = new AblyService();
-        this.AblyClass.connect();
+        this.AblyClass = new AblyService(220, this.handleMessageFromAbly);
+        this.connected = this.AblyClass.connect();
     }
 }
 </script>
--- src/services/ably.service.js
+++ src/services/ably.service.js
@@ -2,23 +2,37 @@
 import * as Ably from 'ably';
 
 export class AblyService {
+    constructor(room, onMessageReceivedCallback) {
+        this.onMessageReceivedCallback = onMessageReceivedCallback;
+        this.room = room;
+    }
+
     connect() {
         // this.onMessageReceivedCallback = onMessageReceivedCallback;
         this.client = new Ably.Realtime({key: 'ZsUvHQ.XqJasQ:PV7-RJ87-fdXRJzYJMPGC_7BvRCHAa2TEc98H6k1H7o'});
+        let connected = false;
+
+        this.client.connection.on('connecting', function() {
+            console.log('Connecting to Ably', 'orange');
+        });
 
         this.client.connection.on('connected', function () {
             console.log('works')
+            connected = true;
         });
 
         this.client.connection.on('failed', function () {
             console.log('nope')
         });
 
-        this.channel = this.client.channels.get('test');
+        this.channel = this.client.channels.get(this.room);
 
         this.channel.subscribe((message) => {
-            this.subscribeCallback(message);
+            // this.subscribeCallback(message);
+            this.onMessageReceivedCallback(message);
         });
+
+        return connected;
     }
 
     sendMessage(name, message) {
